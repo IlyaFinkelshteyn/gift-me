70c5b4b439f0a34528cf46cad62b7ca56a28f914
Simon Thibert <simon.thibert@arhs-data.com>
2016-10-18 22:09:34 +0200

    Change banner

--- tools/tasks/seed/print.banner.ts
+++ tools/tasks/seed/print.banner.ts
@@ -4,15 +4,10 @@ import * as util from 'gulp-util';
 import { join } from 'path';
 
 export = (done: any) => {
-  let bannerPath = join(Config.TOOLS_DIR, 'config', 'banner.txt');
-  if (require('supports-color').has256) {
-    bannerPath = join(Config.TOOLS_DIR, 'config', 'banner-256.txt');
-  }
-  readFile(bannerPath, (e, content) => {
+  readFile(join(Config.TOOLS_DIR, 'config', 'banner.txt'), (e, content) => {
     if (!e) {
       console.log(util.colors.green(content.toString()));
     }
     done();
   });
 };
-
